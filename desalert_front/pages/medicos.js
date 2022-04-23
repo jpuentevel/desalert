2df36e83eb5a308f4e5d69f36fa44c8a2d9d0420
jpuentevel <jpuentevelasquez@gmail.com>
2022-04-23 17:16:02 -0500

    Añadido bootstrap y carpeta components

---
+++ desalert_front/pages/medicos.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const Medicos = () => {
+    return (
+        <>
+            <h1>Medicos</h1>
+            <div className="container" ></div>
+        </>
+    )
+}
