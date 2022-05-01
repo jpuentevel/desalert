dc0fd86054dc90e46197c82b4d9f41b71eb3b7d9
jpuentevel <>
2022-05-01 18:12:23 +0000

    class Medico en proceso

---
+++ desalert_front/classes/Medico.js
@@ -0,0 +1,8 @@
+class Medico {
+  constructor(nombre="", edad=0, id="", especialidad="") {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.id = id;
+    this.especialidad = especialidad;
+  }
+}
--- desalert_front/components/SoloDePrueba.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SoloDePrueba = () => {
-  return (
-    <div>SoloDePrueba</div>
-  )
-}
